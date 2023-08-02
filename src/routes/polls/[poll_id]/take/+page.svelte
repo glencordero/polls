cc0125d23e1c8e5310e3444fddffaa35219fdd4b
glencordero <glen.cordero@gmail.com>
2023-08-01 17:22:50 -0700

    dynamic routes established and poll option display enabled

--- src/routes/polls/[poll_id]/take/+page.svelte
+++ src/routes/polls/[poll_id]/take/+page.svelte
@@ -11,7 +11,11 @@
     {poll.question}
 </h2>
 
-{#each poll.options as option}
-<p>{option.answer}</p>
+{#each poll.options as option, index}
+<input type="radio" name="choice" >
+<label for="{index + 1}">{option.answer}</label><br>
 {/each}
 
+<br>
+<button type=button>Submit</button>
+  
